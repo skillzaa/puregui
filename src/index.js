33bf119c8a150125d2d1e4aa3da9489b5201fb4e
skillzaa <skillzaa.com@gmail.com>
2022-03-11 15:22:21 +0500

    almost almost prototype

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
 import {Bilzaa2d} from "../node_modules/bilzaa2d/src/bilzaa2d/index.js";
-import createText from "./forms/createText.js";
-import settings from "./forms/settings.js";
+
+import testMenu from "./menus/testMenu.js";
+
 let bilzaa = new Bilzaa2d("bilzaa");
 bilzaa.init();
 bilzaa.draw();
 console.log("bilzaa",bilzaa);
 
-///////////////////////////////////////////////////////
-let createTextBtn = document.getElementById("createTextBtn");
-createTextBtn.addEventListener("click",function(){
-    createText(bilzaa);
-});
-///////////////////////////////////////////////////////
-///////////////////////////////////////////////////////
-let settingsBtn = document.getElementById("settingsBtn");
-settingsBtn.addEventListener("click",function(){
-    settings(bilzaa);
-});
-///////////////////////////////////////////////////////
+////////---use query selector and add this on class
 document.getElementById("navMenu").addEventListener("mouseleave",function(e){
 e.target.style.display = "none";
 });
+
 ///////////////////////////////////////////////////////
-let navMenuBtn = document.getElementById("navMenuBtn");
-navMenuBtn.addEventListener("click",function(e){
-    let d = document.getElementById("navMenu");
-
-    d.style.display = "block";
-    // Try d.parent
-    d.style.left = `${navMenuBtn.getBoundingClientRect().left}px`;
-    d.style.top = `${(navMenuBtn.getBoundingClientRect().bottom + 5)}px`;
-
-});
+testMenu(bilzaa);
 ///////////////////////////////////////////////////////
